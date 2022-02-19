1ed664abc8e1fdfe7f8041caeab46522efa40a63
amanmdesai <amanmukeshdesai@gmail.com>
2022-02-19 18:54:19 +0530

    Add

--- Plots/xsection_plots.py
+++ Plots/xsection_plots.py
@@ -14,7 +14,7 @@ def xsect_vs_mt(  x,  par ):
     # par[2] = a_1
     # par[3] = a_2
     sigma= (par[1])*((par[0]/x[0])**4)*(1+par[2]*(x[0]-par[0])/(par[0]) + par[3]*(((x[0]-par[0])/(par[0]))**2) )
-    return sigma
+    return sigma_mt
 
 
 #https://twiki.cern.ch/twiki/pub/LHCPhysics/TtbarNNLO/rootsdep-param.py.txt
@@ -26,5 +26,33 @@ def xsect_vs_sqrts(  x,  par ):
     sqrts = x[0] * 1000.
     logE  = log( sqrts/14000. )
     logE2 = logE*logE
-    sigma = par[0] + sqrts * ( par[1] + par[3] * logE + par[4] * logE2 ) + sqrts * sqrts * ( par[2] + par[5] * logE + par[6] * logE2 )
-    return sigma
+    sigma_s = par[0] + sqrts * ( par[1] + par[3] * logE + par[4] * logE2 ) + sqrts * sqrts * ( par[2] + par[5] * logE + par[6] * logE2 )
+    return sigma_s
+    
+    
+import numpy as np
+import matplotlib.pyplot as plt
+
+    
+X = np.linspace(1.0, 14.0, num=1000)
+sigma_sqrts = []
+energy=[]
+sigma_py=[]
+for i in X:
+	if i >0:
+		#energy = 13/i
+		sigma_s = xsect_vs_sqrts(  [i],  [-18.8,0.0039,4.95e-6,-0.004820,0.00193168,1.68e-6,-1.799e-6] )
+		if sigma_s>0:
+			print(i , sigma_s)
+
+			#plt.plot(i, sigma_s, 'o')
+
+#plt.yscale('log')
+#plt.show()
+
+
+#to run the code use python xsection_plots.py > outfile.dat
+
+
+
+
